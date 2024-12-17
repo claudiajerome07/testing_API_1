2849314fd1b2fc1a3c17b9c474e206817b8ce3a6
claudiajerome07 <claudiajerome07@gmail.com>
2024-12-17 20:12:26 +0530

    API testing 1

--- index.js
+++ index.js
@@ -30,15 +30,32 @@
 //
 // Purpose:
 // Help teachers retrieve and analyze student performance efficiently.
-
-
 const express = require('express');
 const { resolve } = require('path');
+const data = require('./data.json')
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+app.use(express.json())
+
+app.post('/students/above-threshold',(req,res)=>{
+  const {threshold} =req.body
+
+  const GreaterThreshold=data.filter((element)=>{
+    return element.total>threshold
+
+  })
+
+  return res.send({
+    count:GreaterThreshold.length,
+    students:GreaterThreshold
+  })
+  
+})
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
